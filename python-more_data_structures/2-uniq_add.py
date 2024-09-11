978f89c7aba5b0196a873ed2a94800c7c3f2a315
CHRIS09MT <christophermoralestorres06@gmail.com>
2024-09-11 14:08:40 -0500

    add all elements of a list

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    result = [i + 1 for i in my_list if my_list.count(i) == 1]
-    return sum(result)
+    if not my_list:
+        return 0
+    return sum(set(my_list))
