450925e8ab14c01a0198a6061b18553dc297eb63
CHRIS09MT <christophermoralestorres06@gmail.com>
2024-09-11 16:54:29 -0500

    print the value more big

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None or len(a_dictionary) == 0:
+        return None
+    
+    max_value = max(a_dictionary.values())
+    for i in a_dictionary:
+        if a_dictionary[i] == max_value:
+            return i
