3187fef77c907519c7ca12705396b0ec8568e2ca
CHRIS09MT <christophermoralestorres06@gmail.com>
2024-09-11 16:55:12 -0500

    correcting the pycodestyle of 10-best_score.py

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     if a_dictionary is None or len(a_dictionary) == 0:
         return None
-    
+
     max_value = max(a_dictionary.values())
     for i in a_dictionary:
         if a_dictionary[i] == max_value:
