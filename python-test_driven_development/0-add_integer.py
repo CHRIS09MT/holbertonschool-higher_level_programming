3e38e357f64c8168fc097a68026885169271af73
CHRIS09MT <christophermoralestorres06@gmail.com>
2024-09-14 18:51:17 -0500

    correcting pycodestyle

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -5,21 +5,22 @@ The function takes two arguments and returns their sum after converting
 any float arguments to integers.
 """
 
+
 def add_integer(a, b=98):
     """Sum two integers.
-    Args: a (int, float): first integer argument | b (int, float): second integer argument, default value 98.
+    Args: a: first integer argument. | b: second integer argument.
     return the sum of the two integers"""
 
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    
+
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    
+
     if a != a or b != b:
         raise ValueError("cannot convert float NaN to integer")
-    
-    if abs(a)==float('inf'):
+
+    if abs(a) == float("inf"):
         raise OverflowError("cannot convert float infinity to integer")
 
     a = int(a)
