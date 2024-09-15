e66824f79a18948c9cd2bbce8ee51b086e1053d9
CHRIS09MT <christophermoralestorres06@gmail.com>
2024-09-15 07:28:51 -0500

    correcting the pycodestyle and add the documentation

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
+
+"""
+Defines a function that prints a formatted name.
+Validates that both inputs are strings.
+"""
+
+
 def say_my_name(first_name, last_name=""):
+    """
+    Prints 'My name is <first_name> <last_name>' after validation.
+    Raises TypeError if first_name or last_name are not strings.
+    """
 
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
-    
+
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    
-    print('My name is {} {}'.format(first_name, last_name))
+
+    print("My name is {} {}".format(first_name, last_name))
