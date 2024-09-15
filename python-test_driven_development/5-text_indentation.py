f0549233bdfe045c59b9c3e3bc3717c7611aeb6d
CHRIS09MT <christophermoralestorres06@gmail.com>
2024-09-15 09:15:56 -0500

    correcting code

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -25,5 +25,7 @@ def text_indentation(text):
             while i + 1 < len(text) and text[i + 1] == " ":
                 i += 1
         i += 1
+        
+    result = result.rstrip()
 
-    print(result.strip())
+    print(result)
