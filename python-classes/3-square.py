d06ecc5ce3d4b2543d43d74818905e712f549ce0
CHRIS09MT <christophermoralestorres06@gmail.com>
2024-09-18 14:58:33 -0500

    print the area of a square

---
+++ python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module that defines an empty class Square."""
+
+
+class Square:
+    """Represents an empty square."""
+
+    def __init__(self, size=0):
+        """Initializes the square with a private size attribute."""
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        """Returns the area of the square."""
+
+        return self.__size * self.__size
