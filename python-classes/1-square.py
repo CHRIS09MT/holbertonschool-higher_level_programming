9d256af7e26e19c50f2f00ae52223b5693905991
CHRIS09MT <christophermoralestorres06@gmail.com>
2024-09-18 13:59:20 -0500

    creating a atribute private

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module that defines an empty class Square."""
+
+
+class Square:
+    """Represents an empty square."""
+
+    def __init__(self, size):
+        """Initializes the square with a private size attribute."""
+        self.__size = size
