c3b55cb6e8b470986369ff22e1bae2fd3f31031f
CHRIS09MT <christophermoralestorres06@gmail.com>
2024-09-18 14:40:03 -0500

    implement validations

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -9,7 +9,7 @@ class Square:
         """Initializes the square with a private size attribute."""
 
         if not isinstance(size, int):
-            raise TypeError("Size must be an integer")
+            raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
 
