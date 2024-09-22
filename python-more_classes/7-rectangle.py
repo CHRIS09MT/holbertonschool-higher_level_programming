0e5890d20e4ed17617396cb1f27317307db41f17
CHRIS09MT <christophermoralestorres06@gmail.com>
2024-09-22 12:59:16 -0500

    print any type

---
+++ python-more_classes/7-rectangle.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python3
+"""Module that defines an empty class Rectangle."""
+
+
+class Rectangle:
+    """Represents an empty rectangle."""
+
+    number_of_instances = 0
+    print_symbol = "#"
+
+    def __init__(self, width=0, height=0):
+        """Initialize the rectangle with optional width and height."""
+
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        """The width of the rectangle"""
+
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set the width of the rectangle"""
+
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """The height of the rectangle."""
+
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set the height of the rectangle"""
+
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """return the area of the rectangle"""
+
+        return self.__height * self.__width
+
+    def perimeter(self):
+        """Return the perimeter of the rectangle"""
+
+        if self.__width == 0 or self.__height == 0:
+            return 0
+
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """Returns a string"""
+
+        if self.width == 0 or self.height == 0:
+            return ""
+
+        symbol = str(self.print_symbol)
+
+        row = symbol * self.width
+        return "\n".join([row] * self.height)
+
+    def __repr__(self):
+        """Return the width and height of the rectangle"""
+
+        return f"Rectangle({self.width}, {self.height})"
+
+    def __del__(self):
+        """Delete the instace of rectangle"""
+
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
