05501cf3976e33785d5ffabfdc5228326fa2d4a3
CHRIS09MT <christophermoralestorres06@gmail.com>
2024-09-26 08:43:06 -0500

    return true if obj is instance of a_class

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,16 +1,7 @@
 #!/usr/bin/python3
-"""A class MyList that inherits from list"""
 
 
-class MyList(list):
-    """Inherits from list and has a method to print a sorted the list"""
+def is_same_class(obj, a_class):
+    """Returns True if obj is exactly an instance of a_class"""
 
-    def print_sorted(self):
-        """Prints the list sorted in ascending order"""
-
-        print(sorted(self))
-
-    def is_same_class(obj, a_class):
-        """Returns True if obj is exactly an instance of a_class"""
-
-        return type(obj) == a_class
+    return type(obj) == a_class
