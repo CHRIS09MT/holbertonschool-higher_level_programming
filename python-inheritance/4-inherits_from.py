2445fa06ff850e130987684bc99122a4d91ddee5
CHRIS09MT <christophermoralestorres06@gmail.com>
2024-09-26 09:11:06 -0500

    check if is instance of a class

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module defines the is_kind_of_class function.
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Check if obj is an instance or inherited /
+    (directly or indirectly) instance of a_class
+    """
+
+    return isinstance(obj, a_class)
