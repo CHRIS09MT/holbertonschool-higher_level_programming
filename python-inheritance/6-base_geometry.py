7afd19c54e774e768a25e3a8cd3e19b2361a4677
CHRIS09MT <christophermoralestorres06@gmail.com>
2024-09-26 09:44:20 -0500

    messenger of exception

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module of geometry"""
+
+
+class BaseGeometry:
+    """empty class"""
+
+    def area(self):
+        """
+        messenger of exception
+        """
+
+        raise Exception("area() is not implemented")
