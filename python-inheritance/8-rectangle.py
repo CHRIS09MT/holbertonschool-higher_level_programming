47ca4af77ccd40c41bb3ea0aab96a3df25a1a0c1
CHRIS09MT <christophermoralestorres06@gmail.com>
2024-09-26 16:23:33 -0500

    correcting functions

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -37,8 +37,8 @@ class Rectangle(BaseGeometry):
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
-        
-    
+
+
     def __str__(self):
-        """String representation of the Rectangle"""
+        """String representation of the rectangle"""
         return f"[Rectangle] {self.__width}/{self.__height}"
