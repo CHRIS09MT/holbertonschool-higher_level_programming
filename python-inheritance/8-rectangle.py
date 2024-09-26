08e7785889807a13820e9e1c619a9c76182923a1
CHRIS09MT <christophermoralestorres06@gmail.com>
2024-09-26 17:47:51 -0500

    changing the mode of import

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,27 +1,7 @@
 #!/usr/bin/python3
 """Module of geometry"""
 
-
-class BaseGeometry:
-    """empty class"""
-
-    def area(self):
-        """
-        messenger of exception
-        """
-
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        Validates that value is an integer and greater than 0
-        """
-
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 class Rectangle(BaseGeometry):
     """
