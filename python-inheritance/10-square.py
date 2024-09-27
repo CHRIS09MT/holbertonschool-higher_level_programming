764fed2463dc2d7e854ad0350dee5c9a2b68b06c
CHRIS09MT <christophermoralestorres06@gmail.com>
2024-09-26 20:08:23 -0500

    print the area of a rectangle

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Square module"""
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """Square class that inherits from Rectangle"""
+
+    def __init__(self, size):
+        """Initialize square with size"""
+
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def área(self):
+        """Return the area of the square"""
+
+        return self.__height * self.__width
+
+    def __str__(self):
+        """String representation of the square"""
+
+        return f"[Square] {self.__size}/{self.__size}"
