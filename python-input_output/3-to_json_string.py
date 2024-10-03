08849ccfe4aa238248da0e618b244554ec9cd66e
CHRIS09MT <christophermoralestorres06@gmail.com>
2024-10-03 10:38:33 -0500

    return a JSON

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import json
+
+"""
+Importing JSON
+"""
+
+
+def to_json_string(my_obj):
+    """
+    Returns the JSON representation of an object.
+    """
+
+    return json.dumps(my_obj)
