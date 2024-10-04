bf9500bf294b5311c3706c2e2af67033a02ba661
CHRIS09MT <christophermoralestorres06@gmail.com>
2024-10-04 18:02:49 -0500

    verify if a dictionary representation of a Student instance

---
+++ python-input_output/10-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+creating a Student class
+"""
+
+
+class Student:
+    """
+    defining a class of a student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        returns the dictionary description with simple data structure
+        """
+
+        if isinstance(attrs, list) and all(isinstance(attr, str) for attr in attrs):
+            filtered_dict = {
+                attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)
+            }
+            return filtered_dict
+        else:
+            return self.__dict__
