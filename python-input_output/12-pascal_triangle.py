e732089d156f3adb64972cc1fbdeb3dae7a16bae
CHRIS09MT <christophermoralestorres06@gmail.com>
2024-10-05 16:38:30 -0500

    verify if a dictionary representation of a Student instance

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -12,15 +12,14 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    triangle = []
+    triangle = [[1]]
 
-    for i in range(n):
+    for i in range(1, n):
+        prev_row = triangle[-1]
         row = [1]
-
-    if i > 0:
         for j in range(1, i):
-            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+            row.append(prev_row[j - 1] + prev_row[j])
         row.append(1)
-    triangle.append(row)
+        triangle.append(row)
 
     return triangle
