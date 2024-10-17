222317d495595d537dc960551e489be47d2369a7
CHRIS09MT <christophermoralestorres06@gmail.com>
2024-10-17 17:32:56 -0500

    implementing functions

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -15,8 +15,14 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         """
         Handles GET requests for /data and /status endpoints.
         """
-
-        if self.path == "/data":
+        
+        if self.path == '/':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b"Hello, this is a simple API!")
+  
+        elif self.path == "/data":
             self.send_response(200)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
