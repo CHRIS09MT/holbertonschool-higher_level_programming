086f0edc1d2825a6a5c710d25b65627e4813326b
CHRIS09MT <christophermoralestorres06@gmail.com>
2024-10-29 16:00:14 -0500

    correcting the function admin_only

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_httpauth import HTTPBasicAuth
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended.exceptions import NoAuthorizationError, InvalidHeaderError
 
 app = Flask(__name__)
 auth = HTTPBasicAuth()
