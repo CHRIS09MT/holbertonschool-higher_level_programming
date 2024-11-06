36638e699e403091b0d59462e7b824d1e6845341
CHRIS09MT <christophermoralestorres06@gmail.com>
2024-11-06 19:18:13 +0000

    creating a new table with datas into

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,16 +1,14 @@
 -- 3 first students in the Batch ID=3
 -- because Batch 3 is the best!
-DROP TABLE IF EXISTS second_table;
-
 CREATE TABLE IF NOT EXISTS second_table (
-       id INT AUTO_INCREMENT PRIMARY KEY,
+       id INT PRIMARY KEY,
        name VARCHAR(50),
        score INT
 );
 
-INSERT INTO second_table (name, score)
+INSERT INTO second_table (id, name, score)
        VALUES
-       ('John', 10),
-       ('Alex', 3),
-       ('Bob', 14),
-       ('George', 8);
+       (1, 'John', 10),
+       (2, 'Alex', 3),
+       (3, 'Bob', 14),
+       (4, 'George', 8);
