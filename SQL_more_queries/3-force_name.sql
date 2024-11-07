177cb72d4525c3230d4ecc5f8b2ade8dda1412cc
CHRIS09MT <christophermoralestorres06@gmail.com>
2024-11-07 16:33:44 +0000

    Crating a table with the descriptions following, id and name

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,9 +1,6 @@
 -- 3 first students in the Batch ID=3
 -- because Batch 3 is the best!
-
-USE ${1};
-
 CREATE TABLE IF NOT EXISTS force_name (
-       id INT PRIMARY KEY,
+       id INT,
        name VARCHAR(256) NOT NULL
 );
