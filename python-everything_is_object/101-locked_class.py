8b760b184c8340c30bc773f366cbb56e9dab85df
CHRIS09MT <christophermoralestorres06@gmail.com>
2024-12-03 17:16:20 -0500

    slots

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
-class LockedClass():
+"""
+class LockedClass
+"""
+
+
+class LockedClass:
+    """
+    A locked class with a single attribute: first_name
+    """
+
     __slots__ = ("first_name",)
